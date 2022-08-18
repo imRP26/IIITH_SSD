5e0cdf4e725ac6694d7b92bbab7191361fa4cd02
Rahul Padhy <104157969+JimHalpert26@users.noreply.github.com>
2022-08-18 17:19:05 +0530

    Delete q2.sql

--- ssd_lab_activity_3/q2.sql
+++
@@ -1,7 +0,0 @@
-use company;
-
-select concat(fname, ' ', minit, ' ', lname) as fullName, ssn, dno, numSubordinates 
-from employee join
-(select distinct super_ssn, count(*) as numSubordinates from employee where 
-super_ssn is not null group by super_ssn) as table1 
-on table1.super_ssn = employee.ssn;

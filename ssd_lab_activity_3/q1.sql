bcc1fb0caa9e63f0664f0a73e2bde3e21e8f0d3b
Rahul Padhy <104157969+JimHalpert26@users.noreply.github.com>
2022-08-18 17:18:56 +0530

    Delete q1.sql

--- ssd_lab_activity_3/q1.sql
+++
@@ -1,8 +0,0 @@
-use company;
-
-select concat(fname, ' ', minit, ' ', lname) as fullName, ssn, dnumber, dname from employee, 
-department,  
-(select essn, sum(hours) as numHours from works_on group by essn having numHours < 40) 
-as table1 
-where table1.essn = employee.ssn and employee.dno = department.dnumber and 
-department.mgr_ssn = employee.ssn;

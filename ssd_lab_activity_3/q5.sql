8b2b4933cd3db27c5d2ae3e4e1bbcbfd0f99f1a6
Rahul Padhy <104157969+JimHalpert26@users.noreply.github.com>
2022-08-18 17:19:39 +0530

    Delete q5.sql

--- ssd_lab_activity_3/q5.sql
+++
@@ -1,9 +0,0 @@
-use company;
-
-select mgr_ssn, dnumber, numDependents from 
-(select essn, count(*) as numDependents from dependent group by essn) as table3
-join
-(select mgr_ssn, dnumber from department where dnumber in 
-(select dnumber from 
-(select dnumber, count(*) as numLocations from dept_locations group by dnumber having 
-numLocations >= 2) as table1)) as table2 on table3.essn = table2.mgr_ssn;

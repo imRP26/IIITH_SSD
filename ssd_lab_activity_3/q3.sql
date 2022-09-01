d2737b520aa96f8593cf38d81ecfb6809f5298d2
Rahul Padhy <104157969+JimHalpert26@users.noreply.github.com>
2022-09-01 12:11:42 +0530

    Delete q3.sql

--- ssd_lab_activity_3/q3.sql
+++
@@ -1,5 +0,0 @@
-use company;
-
-select count(pno) as numProjects from works_on where essn in 
-(select mgr_ssn from department where dnumber in
-(select dnum from project where pname = 'ProductY'));
